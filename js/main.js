111774ae4520420ec6be7af6ddaa77449a444bd2
Noriana <norivhc@gmail.com>
2023-07-19 17:39:02 -0300

    add storage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const carrito = [];
+let carrito = [];
 
 async function fetchData() {
   try {
@@ -7,17 +7,17 @@ async function fetchData() {
     console.log(data);
 
     const btns = document.querySelectorAll("button");
-    btns.forEach(button => {
-      button.addEventListener("click", e => {
+    btns.forEach((button) => {
+      button.addEventListener("click", (e) => {
         const categoria = e.target.innerText;
-       if(categoria === "Todos") {
-        crearCatalogo(data)
-       } else {
-         const productosFiltrados = data.filter(producto => producto.categoria === categoria);
-         crearCatalogo(productosFiltrados);
-
-       }
-        
+        if (categoria === "Todos") {
+          crearCatalogo(data);
+        } else {
+          const productosFiltrados = data.filter(
+            (producto) => producto.categoria === categoria
+          );
+          crearCatalogo(productosFiltrados);
+        }
       });
     });
 
@@ -30,74 +30,169 @@ async function fetchData() {
 fetchData();
 
 const body = document.querySelector("body");
+const divTotal = document.querySelector(".total")
+
 const contCatalogo = document.createElement("div");
-contCatalogo.classList.add("contCatalogo")
+contCatalogo.classList.add("contCatalogo");
 body.appendChild(contCatalogo);
 
-const seccionCarrito = document.querySelector(".carrito")
+const seccionCarrito = document.querySelector(".carrito");
 
 function crearCatalogo(array) {
-  contCatalogo.innerHTML = ""; 
+  contCatalogo.innerHTML = "";
 
-  array.forEach(producto => {
+  array.forEach((producto) => {
     const card = document.createElement("div");
-    card.classList.add("card"); 
-    contCatalogo.appendChild(card)
+    card.classList.add("card");
+    contCatalogo.appendChild(card);
+
     const nombre = document.createElement("div");
-    nombre.innerText = `${producto.nombre}`
-    nombre.classList.add("nombre")
-    card.appendChild(nombre)
+    nombre.innerText = `${producto.nombre}`;
+    nombre.classList.add("nombre");
+    card.appendChild(nombre);
 
     const precio = document.createElement("div");
-    precio.innerText = `${producto.precio}`
-    precio.classList.add("precio")
-    card.appendChild(precio)
+    precio.innerText = `${producto.precio}`;
+    precio.classList.add("precio");
+    card.appendChild(precio);
+
     const categoria = document.createElement("div");
-    categoria.innerText = `${producto.categoria}`
-    categoria.classList.add("categoria")
-    card.appendChild(categoria)
+    categoria.innerText = `${producto.categoria}`;
+    categoria.classList.add("categoria");
+    card.appendChild(categoria);
+
     const imagen = document.createElement("img");
     imagen.src = producto.img;
     imagen.classList.add("img");
-    card.appendChild(imagen)
+    card.appendChild(imagen);
 
-    const botonCompra = document.createElement("button")
-    botonCompra.classList.add("botonCompra")
-    card.appendChild(botonCompra)
-    botonCompra.innerText = "Agregar al carrito"
+    const botonCompra = document.createElement("button");
+    botonCompra.classList.add("botonCompra");
+    card.appendChild(botonCompra);
+    botonCompra.innerText = "Agregar al carrito";
 
     botonCompra.addEventListener("click", (e) => {
-      const indiceProducto = carrito.findIndex(item => item.nombre === producto.nombre);
+      const indiceProducto = carrito.findIndex(
+        (item) => item.nombre === producto.nombre
+      );
+
       if (indiceProducto !== -1) {
-        // Object with the same name already exists, increase cantidad
         carrito[indiceProducto].cantidad++;
-    
-        const cantidadProductoCarrito = document.querySelector(`#cantidadProductoCarrito-${indiceProducto}`);
+
+        const cantidadProductoCarrito = document.querySelector(
+          `#cantidadProductoCarrito-${indiceProducto}`
+        );
+        const subtotalCarrito = document.querySelector(
+          `#subtotal-${indiceProducto}`
+        );
+
         if (cantidadProductoCarrito) {
-          cantidadProductoCarrito.innerText = carrito[indiceProducto].cantidad;
+          let subtotal =
+            carrito[indiceProducto].cantidad * carrito[indiceProducto].precio;
+          carrito[indiceProducto].subtotal = subtotal;
+          cantidadProductoCarrito.innerText = `cantidad: ${carrito[indiceProducto].cantidad}`;
+          subtotalCarrito.innerText = `subtotal: ${subtotal}`;
+          actualizarTotal();
         }
       } else {
-        // Object doesn't exist, add new object to the carrito array
         carrito.push({
           nombre: producto.nombre,
-          cantidad: 1
+          cantidad: 1,
+          precio: producto.precio,
+          subtotal: producto.precio,
         });
-    
+
         const nuevoIndiceProducto = carrito.length - 1;
-    
+
+        const productoCarrito = document.createElement("div");
+        productoCarrito.classList.add("producto-carrito");
+        seccionCarrito.appendChild(productoCarrito);
+
         const nombreCarrito = document.createElement("p");
+        nombreCarrito.classList.add("nombre-carrito");
         nombreCarrito.innerText = producto.nombre;
-        seccionCarrito.appendChild(nombreCarrito);
-    
+        productoCarrito.appendChild(nombreCarrito);
+
         const cantidadProductoCarrito = document.createElement("p");
+        cantidadProductoCarrito.classList.add("cantidad-carrito");
         cantidadProductoCarrito.id = `cantidadProductoCarrito-${nuevoIndiceProducto}`;
-        cantidadProductoCarrito.innerText = carrito[nuevoIndiceProducto].cantidad;
-        seccionCarrito.appendChild(cantidadProductoCarrito);
+        cantidadProductoCarrito.innerText = `cantidad: ${carrito[nuevoIndiceProducto].cantidad}`;
+        productoCarrito.appendChild(cantidadProductoCarrito);
+
+        const subtotal = document.createElement("p");
+        subtotal.classList.add("subtotal-carrito");
+        subtotal.id = `subtotal-${nuevoIndiceProducto}`;
+        subtotal.innerText = `subtotal: ${carrito[nuevoIndiceProducto].subtotal}`;
+        productoCarrito.appendChild(subtotal);
+
+        actualizarTotal();
       }
     });
-  })
+  });
+}
+
+function actualizarTotal() {
+  const totalElement = document.querySelector(".total");
+
+  let totalPrecio = 0;
+  carrito.forEach((item) => {
+    totalPrecio += item.subtotal;
+  });
+
+  if (totalElement) {
+    totalElement.innerText = `Precio total: ${totalPrecio}`;
+  } else {
+    const total = document.createElement("p");
+    total.innerText = `Precio total: ${totalPrecio}`;
+    total.classList.add("total");
+    divTotal.appendChild(total);
   }
+}
+
+function guardarCarritoEnLocalStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+window.addEventListener("beforeunload", guardarCarritoEnLocalStorage);
+
+// Cargar carrito desde el localStorage al cargar la página
+function cargarCarritoDesdeLocalStorage() {
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    actualizarCarrito();
+  }
+}
+
+// Actualizar el carrito en el DOM
+function actualizarCarrito() {
+  seccionCarrito.innerHTML = "";
+
+  carrito.forEach((producto, index) => {
+    const productoCarrito = document.createElement("div");
+    productoCarrito.classList.add("producto-carrito");
+    seccionCarrito.appendChild(productoCarrito);
+
+    const nombreCarrito = document.createElement("p");
+    nombreCarrito.classList.add("nombre-carrito");
+    nombreCarrito.innerText = producto.nombre;
+    productoCarrito.appendChild(nombreCarrito);
+
+    const cantidadProductoCarrito = document.createElement("p");
+    cantidadProductoCarrito.classList.add("cantidad-carrito");
+    cantidadProductoCarrito.id = `cantidadProductoCarrito-${index}`;
+    cantidadProductoCarrito.innerText = `cantidad: ${producto.cantidad}`;
+    productoCarrito.appendChild(cantidadProductoCarrito);
+
+    const subtotal = document.createElement("p");
+    subtotal.classList.add("subtotal-carrito");
+    subtotal.id = `subtotal-${index}`;
+    subtotal.innerText = `subtotal: ${producto.subtotal}`;
+    productoCarrito.appendChild(subtotal);
+  });
+
+  actualizarTotal();
+}
 
 
+window.addEventListener("DOMContentLoaded", cargarCarritoDesdeLocalStorage);
 
 
